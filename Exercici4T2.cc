16f9bd0a67f0a52a4b54aaf60fc10ddc38852916
Dídac Molina <didac.molina@estudiantat.upc.edu>
2026-01-11 13:26:21 +0100

    Add files via upload

---
+++ Exercici4T2.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+using namespace std;
+
+typedef vector<vector<int>> Matriu;
+
+// Funció a implementar
+vector<pair<int,int>> deps_ordenades(const Matriu& M);
+
+int main() {
+    int r, c;
+    while (cin >> r >> c) {
+        Matriu M(r, vector<int>(c));
+        for (int i = 0; i < r; ++i) {
+            for (int j = 0; j < c; ++j) cin >> M[i][j];
+        }
+
+        vector<pair<int,int>> res = deps_ordenades(M);
+
+        if (res.empty()) cout << "cap\n";
+        else {
+            for (auto [i, j] : res) cout << (i + 1) << " -> " << (j + 1) << "\n";
+        }
+    }
+    return 0;
+}
